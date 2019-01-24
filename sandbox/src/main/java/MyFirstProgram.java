df26bf46c45fb5210da3ba90f92bf16414657cc4
khhariz <46896691+khhariz@users.noreply.github.com>
2019-01-24 16:22:18 +0200

    Добавлено несколько функций

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,24 @@
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-                System.out.println("Hello, World");
+        hello("Kris");
+
+        double length = 7;
+        System.out.println("Площадь равна " +  area(length));
     }
 
+
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+
+
+
+    public static double area(double l){
+         return l*l;
 }
+
+}
+
+
